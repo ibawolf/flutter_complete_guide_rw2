55a7185f1fc45918332a122d31d87573f77f6417
Ron Wolf <RonaldBWolf@wolfamily.net>
2024-12-30 09:27:19 -0600

    added bread crumbs to delete expense in expense tracker

--- 003_expense_tracker/lib/expenses.dart
+++ 003_expense_tracker/lib/expenses.dart
@@ -44,9 +44,28 @@ class _ExpenseState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(
+              () {
+                _registeredExpenses.insert(expenseIndex, expense);
+              },
+            );
+          },
+        ),
+      ),
+    );
   }
 
   @override
